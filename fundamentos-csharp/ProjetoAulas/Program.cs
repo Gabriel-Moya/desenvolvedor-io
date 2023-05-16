ae2032c6a9f781bbcbe8899ad09012c261d78b40
Gabriel Moya <gabrielmoya123@gmail.com>
2023-05-16 10:47:25 -0400

    If else if

--- fundamentos-csharp/ProjetoAulas/Program.cs
+++ fundamentos-csharp/ProjetoAulas/Program.cs
@@ -222,18 +222,67 @@ Console.WriteLine(divisao);
 
 
 
-var stack = new Stack<string>();
-stack.Push("Gabriel");
-stack.Push("Moya");
+// var stack = new Stack<string>();
+// stack.Push("Gabriel");
+// stack.Push("Moya");
 
-var nome = stack.Pop();
-var nome1 = stack.Pop();
-Console.WriteLine(nome);
-Console.WriteLine(nome1);
-
-
+// var nome = stack.Pop();
+// var nome1 = stack.Pop();
+// Console.WriteLine(nome);
+// Console.WriteLine(nome1);
 
 // foreach (var item in stack)
 // {
 //     Console.WriteLine(item);
 // }
+
+// MODULO 7 - Aula 1
+// var diaDaSemana = 0;
+// var diaDeTrabalho = false;
+
+// if (diaDaSemana == 0 && diaDeTrabalho == true)
+// {
+//     Console.WriteLine("Hoje é domingo");
+// }
+// else if (diaDaSemana == 0 && diaDeTrabalho == false)
+// {
+//     Console.WriteLine("Hoje é domingo, mas não é dia de trabalho");
+// }
+// else
+// {
+//     Console.WriteLine("Hoje não é domingo");
+// }
+
+var diaDaSemana = 10;
+if (diaDaSemana == 0)
+{
+    Console.WriteLine("Hoje é domingo");
+}
+else if (diaDaSemana == 1)
+{
+    Console.WriteLine("Hoje é segunda");
+}
+else if (diaDaSemana == 2)
+{
+    Console.WriteLine("Hoje é terça");
+}
+else if (diaDaSemana == 3)
+{
+    Console.WriteLine("Hoje é quarta");
+}
+else if (diaDaSemana == 4)
+{
+    Console.WriteLine("Hoje é quinta");
+}
+else if (diaDaSemana == 5)
+{
+    Console.WriteLine("Hoje é sexta");
+}
+else if (diaDaSemana == 6)
+{
+    Console.WriteLine("Hoje é sábado");
+}
+else
+{
+    Console.WriteLine("Dia da semana inválido");
+}
