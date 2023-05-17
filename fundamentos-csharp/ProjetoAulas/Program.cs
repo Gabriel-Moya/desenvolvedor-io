ef27a8c07e8d6334a5236359015a4a24bf2d4de5
Gabriel Moya <gabrielmoya123@gmail.com>
2023-05-17 11:48:48 -0400

    O que é uma classe

--- fundamentos-csharp/ProjetoAulas/Program.cs
+++ fundamentos-csharp/ProjetoAulas/Program.cs
@@ -8,7 +8,20 @@ namespace Application
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AulaClasses();            
+        }
+
+        private static void AulaClasses()
+        {
+            var resultado = Cadastro.Calculos.SomarNumeros(2, 3);
+            Console.WriteLine(resultado);
+
+            // var produto = new Cadastro.Produto();
+            // produto.SetId(1);
+            // produto.Descricao = "Teclado";
+
+            // produto.ImprimirDescricao();
+            // Console.WriteLine(produto.GetId());
         }
     }
 }
