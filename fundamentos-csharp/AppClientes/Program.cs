15163d41e8a87118bd3e3ea7df7d45a56f539793
Gabriel Moya <gabrielmoya123@gmail.com>
2023-05-28 18:05:12 -0400

    Implementando funcionalidades

--- fundamentos-csharp/AppClientes/Program.cs
+++ fundamentos-csharp/AppClientes/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace AppClientes;
+﻿using Repositorio;
+using System.Globalization;
+
+namespace AppClientes;
 
 class Program
 {
+    static ClienteRepositorio _clienteRepositorio = new ClienteRepositorio();
+
     static void Main(string[] args)
     {
+        var cultura = new CultureInfo("pt-BR");
+        Thread.CurrentThread.CurrentCulture = cultura;
+        Thread.CurrentThread.CurrentUICulture = cultura;
         while (true)
         {
             Menu();
@@ -24,5 +32,54 @@ class Program
         Console.WriteLine("4 - Excluir Cliente");
         Console.WriteLine("5 - Sair");
         Console.WriteLine("--------------------");
+
+        EscolherOpcao();
+    }
+
+    static void EscolherOpcao()
+    {
+        Console.Write("Escolha uma opção: ");
+
+        var opcao = Console.ReadLine();
+
+        switch (int.Parse(opcao))
+        {
+            case 1:
+                {
+                    _clienteRepositorio.CadastrarCliente();
+                    Menu();
+                    break;
+                }
+            case 2:
+                {
+                    _clienteRepositorio.ExibirClientes();
+                    Menu();
+                    break;
+                }
+            case 3:
+                {
+                    _clienteRepositorio.EditarCliente();
+                    Menu();
+                    break;
+                }
+            case 4:
+                {
+                    _clienteRepositorio.ExcluirCliente();
+                    Menu();
+                    break;
+                }
+            case 5:
+                {
+                    // _clienteRepositorio.GravarDadosClientes();
+                    Environment.Exit(0);
+                    break;
+                }
+            default:
+                {
+                    Console.Clear();
+                    Console.WriteLine("Opção Inválida!"); 
+                    break;
+                }
+        }
     }
 }
